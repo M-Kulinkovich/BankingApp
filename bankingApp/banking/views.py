c2043ef8fdafa7107e0b188f060400312766d014
M-Kulinkovich <wait_z@mail.ru>
2023-04-04 16:07:55 +0300

    ADD currency_data API adn logo to HomePage

--- bankingApp/banking/views.py
+++ bankingApp/banking/views.py
@@ -1,3 +1,7 @@
+import json
+import os
+
+import requests
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import reverse_lazy
@@ -8,6 +12,28 @@ from banking.forms import RegisterUserForm
 
 class IndexPage(TemplateView):
     template_name = 'banking/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        url = "https://api.apilayer.com/currency_data/live?source=USD&currencies=EUR,GBP,BYN,PLN"
+        headers = {
+            "apikey": os.environ.get('API_KEY')
+        }
+        response = requests.get(url, headers=headers)
+        data = response.json()
+
+        eur_rate = data['quotes']['USDEUR']
+        gbp_rate = data['quotes']['USDGBP']
+        byn_rate = data['quotes']['USDBYN']
+        pln_rate = data['quotes']['USDPLN']
+
+        context['eur_rate'] = eur_rate
+        context['gbp_rate'] = gbp_rate
+        context['byn_rate'] = byn_rate
+        context['pln_rate'] = pln_rate
+
+        return context
 
 
 class LoginUser(LoginView):
